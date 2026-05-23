a29a89251161dd0b136d19c64598e97e0b69d81b
Mariel <casoldam@gmail.com>
2026-05-22 20:19:41 -0500

    LogIn: Form de iniciar sesion + user y pass (#19)

--- lib/pages/log_in/log_in_page.dart
+++ lib/pages/log_in/log_in_page.dart
@@ -2,10 +2,95 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'log_in_controller.dart';
 
-class LogInPage extends StatelessWidget {
+class LogInPage extends StatefulWidget {
+  LogInPage({super.key});
+
+  @override
+  State<LogInPage> createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
   LogInController control = Get.put(LogInController());
 
-  LogInPage({super.key});
+
+  Widget _form(BuildContext context, colors, textTheme){
+    return Expanded(
+      flex: 5,
+      child:
+      Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(28),
+        decoration: BoxDecoration(
+          color: colors.surface,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(36),
+            topRight: Radius.circular(36),
+          ),
+        ),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Iniciar Sesión',
+              style: textTheme.headlineSmall?.copyWith(
+              color: colors.primary,
+              fontWeight: FontWeight.w700,
+              fontSize: 22.0,
+            ),
+          ),
+
+          Text('Ingresa con tu cuenta para continuar',
+              style: textTheme.bodyMedium?.copyWith(
+              color: colors.onSurfaceVariant,
+              fontSize: 16.0,
+            ),
+          ),
+
+        SizedBox(height: 20),
+
+        //User
+        TextField(
+          controller: control.usernameController,
+          decoration: InputDecoration(
+            labelText: "Usuario",
+            labelStyle: TextStyle(
+              color: colors.onSurface
+            ),
+            hintText: 'Tu usuario',
+            border: OutlineInputBorder(),
+          ),
+        ),
+
+        SizedBox(height: 10),
+
+        //Password
+        TextField(
+          controller: control.passwordController,
+          obscureText: control.isPasswordHidden,
+          decoration: InputDecoration(
+            labelText: "Contraseña",
+            labelStyle: TextStyle(
+              color: colors.onSurface
+            ),
+            border: OutlineInputBorder(),
+            suffixIcon: IconButton(
+              icon: Icon(
+                control.isPasswordHidden ? Icons.visibility : Icons.visibility_off,
+              ),
+
+              onPressed: () {
+                setState(() {
+                  control.togglePasswordVisibility();
+                  });
+              },
+            )
+          ),
+        ),
+
+        ],),
+      ),
+    );
+  }
 
   Widget _logo(BuildContext context, colors, textTheme){
     // REEMPLAZAR POR ASSET DE LOGO, ESTO ESTA XD
@@ -35,14 +120,14 @@ class LogInPage extends StatelessWidget {
 
   Widget _centerText(BuildContext context, colors, textTheme){
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 20),
+      padding: EdgeInsets.symmetric(vertical: 60),
       width: double.infinity,
       child:
       Column(
         children: [
           _logo(context, colors, textTheme),
 
-          SizedBox(height: 24),
+          SizedBox(height: 14),
 
           Text("ReportaYA",
           style: textTheme.headlineLarge?.copyWith(
@@ -66,7 +151,7 @@ class LogInPage extends StatelessWidget {
 
   Widget _background(BuildContext context, colors){
     return Expanded(
-            flex: 30,
+            //flex: 30,
             child: Container(
               width: double.infinity,
               padding: const EdgeInsets.symmetric(
@@ -74,12 +159,12 @@ class LogInPage extends StatelessWidget {
               ),
               decoration: BoxDecoration(
                 gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
                   colors: [
                     colors.primary,
                     colors.secondary,
                   ],
-                  //begin: Alignment.topLeft,
-                  //end: Alignment.bottomRight
                 ),
               ),
         )
@@ -89,12 +174,13 @@ class LogInPage extends StatelessWidget {
   Widget _buildBody(BuildContext context, colors, textTheme) {
     return SafeArea(child:
       Stack(children: [
-        Column(children: [
-          _background(context, colors),
-          ],
-          ),
+        _background(context, colors),
 
-        _centerText(context, colors, textTheme),
+        Column(children: [
+          _centerText(context, colors, textTheme),
+          _form(context, colors, textTheme),
+        ],),
+
         ],
       )
     );
