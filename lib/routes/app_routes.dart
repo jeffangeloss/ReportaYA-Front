3c2d7690c70171eabbdfb081737e343b5e101c57
Mariel <casoldam@gmail.com>
2026-06-04 00:21:08 -0500

    CU-03 - Recuperar contraseña implementado en local

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -5,6 +5,8 @@ import '../screens/auth/register_screen.dart';
 import '../screens/ciudadano/main_tabs.dart';
 import '../screens/operador/home_screen_operador.dart';
 import '../screens/tecnico/home_screen_tecnico.dart';
+import '../screens/auth/recover_password_screen.dart';
+import '../screens/auth/reset_password_screen.dart';
 
 class AppRoutes {
   static const String splash = '/';
@@ -13,6 +15,8 @@ class AppRoutes {
   static const String mainTabs = '/main';
   static const String homeOperador = '/operador';
   static const String homeTecnico = '/tecnico';
+  static const String recoverPassword = '/recover-password';
+  static const String resetPassword = '/reset-password';
 
   static List<GetPage> get pages => [
         GetPage(name: splash, page: () => const SplashScreen()),
@@ -21,5 +25,7 @@ class AppRoutes {
         GetPage(name: mainTabs, page: () => const MainTabs()),
         GetPage(name: homeOperador, page: () => const HomeScreenOperador()),
         GetPage(name: homeTecnico, page: () => const HomeScreenTecnico()),
+        GetPage(name: recoverPassword, page: () => const RecoverPasswordScreen()),
+        GetPage(name: resetPassword, page: () => const ResetPasswordScreen()),
       ];
 }
